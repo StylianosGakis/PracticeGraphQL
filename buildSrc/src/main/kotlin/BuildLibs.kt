7b74cc3612f8165c0bccf425c794ff28431db49b
stylianosgakis <stylianos.gakis98@gmail.com>
2022-05-06 16:13:26 +0300

    Bump agp

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,5 +1,5 @@
 object BuildLibs {
-    const val AndroidGradlePlugin = "com.android.tools.build:gradle:7.0.1"
+    const val AndroidGradlePlugin = "com.android.tools.build:gradle:7.1.3"
     const val KotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${CommonVersions.kotlin}"
     const val ApolloPlugin = "com.apollographql.apollo3:apollo-gradle-plugin:${CommonVersions.apollo}"
     const val HiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:${CommonVersions.hilt}"
